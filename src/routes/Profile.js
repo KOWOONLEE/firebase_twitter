554aa135dfb0d586f61b43789f5e329619683ef5
KOWOONLEE <hhh3901@gmail.com>
2023-03-30 23:52:11 +0900

    Rev: profile css

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../color";
 import { auth, dbService } from "../myBase";
 import { collection, where, query, getDocs, orderBy } from "firebase/firestore";
 import { updateProfile } from "firebase/auth";
@@ -50,18 +52,75 @@ const Profile = ({ refreshUser, userObj }) => {
     }
   };
   return (
-    <>
-      <form onSubmit={onSubmitProfile}>
-        <input
-          type="text"
-          placeholder="Display Name"
-          onChange={onChangeProfile}
-          value={displayName}
-        />
-        <input type="submit" value="Update Profile" />
-      </form>
-      <button onClick={onLogOutClick}>Log Out</button>
-    </>
+    <StyledProfile>
+      <div className="editForm">
+        <form onSubmit={onSubmitProfile}>
+          <input
+            type="text"
+            placeholder="Display Name"
+            onChange={onChangeProfile}
+            value={displayName}
+            className="editProfileInput"
+          />
+          <input
+            type="submit"
+            value="Update Profile"
+            className="editProfileSubmit"
+          />
+        </form>
+        <button onClick={onLogOutClick}>Log Out</button>
+      </div>
+    </StyledProfile>
   );
 };
 export default Profile;
+
+const StyledProfile = styled.div`
+  display: inline-block;
+  width: 100%;
+  height: 80vh;
+  justify-content: center;
+  padding-top: 10px;
+  text-align: center;
+
+  .editForm {
+    display: inline-block;
+    width: 30vw;
+    background-color: white;
+    border-radius: 10px;
+
+    form {
+      text-align: center;
+
+      input {
+        width: 25vw;
+        border-radius: 20px;
+        margin-top: 10px;
+      }
+    }
+    button {
+      width: 25vw;
+      height: 4vh;
+      border: none;
+      border-radius: 20px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      background-color: ${theme.pink};
+      cursor: pointer;
+    }
+  }
+  .editProfileInput {
+    height: 5vh;
+    background-color: ${theme.beige};
+    border: 1px solid ${theme.red};
+    border-radius: 20px;
+    text-align: center;
+  }
+  .editProfileSubmit {
+    height: 4vh;
+    background-color: ${theme.bg};
+    border: none;
+    cursor: pointer;
+    margin-bottom: 5vh;
+  }
+`;
